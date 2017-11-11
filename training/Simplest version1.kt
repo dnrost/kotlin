b9431126a61e6ae3c3d36cbed53b9fbc576c56dc
Rossetto <dnrost@gmail.com>
2017-11-10 23:33:00 -0200

    start

---
+++ training/Simplest version1.kt
@@ -0,0 +1,10 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    for (x in args)
+    	println(x)
+}
